2db7618a015817f33d9a27e3c9ed6a0b5ab8a04f
M1sterDonut <M1sterDonut>
2022-05-21 21:36:43 +0000

    Add driver file to datalake
    Includes schema and transformations as per requirements

---
+++ formula1/ingestion/4.ingest_drivers_file.py
@@ -0,0 +1,66 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ### Step 1 - Read file using Spark DataFrame API
+
+# COMMAND ----------
+
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DateType
+
+# COMMAND ----------
+
+name_schema = StructType([StructField('forename', StringType(), nullable=False),\
+                          StructField('surname', StringType(), nullable=False)])
+
+# COMMAND ----------
+
+driver_schema = StructType([StructField('driverId', IntegerType(), nullable=False),\
+                            StructField('driverRef', IntegerType(), nullable=False),\
+                            StructField('number', IntegerType(), nullable=True),\
+                            StructField('code', IntegerType(), nullable=True),\
+                            StructField('name', name_schema),\
+                            StructField('dob', DateType(), nullable=True),\
+                            StructField('nationality', StringType(), nullable=True),\
+                            StructField('url', StringType(), nullable=False)])
+
+# COMMAND ----------
+
+drivers_df = spark.read \
+    .schema(driver_schema) \
+    .json('/mnt/formula123dl/raw/drivers.json')
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Step 2 - Rename & add new columns
+# MAGIC 1. rename driverId & driverRef
+# MAGIC 2. add ingestion date
+# MAGIC 3. concat fore and surname
+
+# COMMAND ----------
+
+from pyspark.sql.functions import current_timestamp, concat, lit, col
+
+transformed_drivers_df = drivers_df.withColumnRenamed('driverId', 'driver_id') \
+                                   .withColumnRenamed('driverRef', 'driver_ref') \
+                                   .withColumn('ingestion_date', current_timestamp()) \
+                                   .withColumn('name',concat(col('name.forename'), lit(' '), col('name.surname')))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Step 3 - Drop unwanted columns
+
+# COMMAND ----------
+
+final_drivers_df = transformed_drivers_df.drop('url')
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Step 4 - Write to parquet
+
+# COMMAND ----------
+
+final_drivers_df.write \
+                .mode('overwrite') \
+                .parquet('/mnt/formula123dl/processed/drivers')

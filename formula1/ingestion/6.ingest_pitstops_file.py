1ce17a4782b4ab7ba27967e9b42d1d3cc93867f2
M1sterDonut <M1sterDonut>
2022-05-22 12:08:59 +0000

    Add pitstops file

---
+++ formula1/ingestion/6.ingest_pitstops_file.py
@@ -0,0 +1,50 @@
+# Databricks notebook source
+#processing multi-line JSON file
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Step 1 - Read multi-line JSON file using Spark DF reader API
+
+# COMMAND ----------
+
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType
+
+# COMMAND ----------
+
+pitstop_schema = StructType([StructField('raceId', IntegerType(), False),\
+                             StructField('driverId', IntegerType(), False),\
+                             StructField('stop', IntegerType(), False),\
+                             StructField('lap', IntegerType(), False),\
+                             StructField('time', StringType(), False),\
+                             StructField('duration', StringType(), True),\
+                             StructField('milliseconds', IntegerType(), True)])
+
+# COMMAND ----------
+
+pitstops_df = spark.read \
+    .schema(pitstop_schema) \
+    .option('multiLine', True) \
+    .json('/mnt/formula123dl/raw/pit_stops.json')
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Step 2 - Rename & add new columns
+
+# COMMAND ----------
+
+from pyspark.sql.functions import current_timestamp, col, lit
+
+final_pitstops_df = pitstops_df.withColumn('ingestion_date', current_timestamp()) \
+                                     .withColumnRenamed('driverId','driver_id') \
+                                     .withColumnRenamed('raceId', 'race_id')
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Step 3 - Write df into DeltaLake as parquet file
+
+# COMMAND ----------
+
+final_pitstops_df.write.mode('overwrite').parquet('/mnt/formula123dl/processed/pitstops')

3658399eb16b3064fa4d456646b34098c8016f25
M1sterDonut <M1sterDonut>
2022-05-22 12:45:54 +0000

    Add qualifying file

---
+++ formula1/ingestion/8.ingest_qualifying_files.py
@@ -0,0 +1,56 @@
+# Databricks notebook source
+#ingesting multiple json files
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Step 1 - Ingest five CSV files / lap_times folder
+
+# COMMAND ----------
+
+#define schema
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType
+
+qualifying_schema = StructType([StructField('qualifyId', IntegerType(), False),\
+                              StructField('raceId', IntegerType(), False),\
+                              StructField('driverId', IntegerType(), False),\
+                              StructField('constructorId', IntegerType(), False),\
+                              StructField('number', IntegerType(), False),\
+                              StructField('position', IntegerType(), True),\
+                              StructField('q1', StringType(), True),\
+                              StructField('q2', StringType(), True),\
+                              StructField('q3', StringType(), True)])
+
+# COMMAND ----------
+
+#ingest data
+#can ingest entire folders or file names with wildcard (*) parts
+
+qualifying_df = spark.read \
+    .schema(qualifying_schema) \
+    .option('multiLine',True) \
+    .json('/mnt/formula123dl/raw/qualifying/qualifying_split_*.json')
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Step 2 - Rename & add new columns
+
+# COMMAND ----------
+
+from pyspark.sql.functions import current_timestamp, col, lit
+
+final_qualifying_df = qualifying_df.withColumn('ingestion_date', current_timestamp()) \
+                                   .withColumnRenamed('qualifyId','qualify_id') \
+                                   .withColumnRenamed('constructorId', 'constructor_id') \
+                                   .withColumnRenamed('driverId','driver_id') \
+                                   .withColumnRenamed('raceId', 'race_id')
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Step 3 - Write df into DeltaLake as parquet file
+
+# COMMAND ----------
+
+final_qualifying_df.write.mode('overwrite').parquet('/mnt/formula123dl/processed/qualifying')
